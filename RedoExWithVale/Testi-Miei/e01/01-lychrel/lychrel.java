0b238e63021b8cb8590bda8223535bbdfc6bcb12
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-02-23 10:15:26 +0100

    LychrelCheck

--- RedoExWithVale/Testi-Miei/e01/01-lychrel/lychrel.java
+++ RedoExWithVale/Testi-Miei/e01/01-lychrel/lychrel.java
@@ -2,21 +2,34 @@ import java.util.Scanner;
 
 public class lychrel {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         int i = in.nextInt();
+        String arg = String.valueOf(i);
 
-        String iStr = String.valueOf(i);
-        String newStr = "";
-        for (int j = iStr.length() - 1; j >= 0; j--) {
-            newStr += iStr.charAt(j);
+        while (!isPalindroma(arg)) {
+
+            String iStr = String.valueOf(i);
+            String newStr = "";
+            for (int j = iStr.length() - 1; j >= 0; j--) {
+                newStr += iStr.charAt(j);
+            }
+            // System.out.println(newStr);
+            i = i + Integer.parseInt(newStr);
+            arg = String.valueOf(i);
+            System.out.println(i);
         }
-        // System.out.println(newStr);
-        i = i + Integer.parseInt(newStr);
-
-        System.out.println(i);
-
         in.close();
 
     }
 
+    public static boolean isPalindroma(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i)) != (str.charAt(str.length() - 1 - i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
